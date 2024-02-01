4a5b7b0f76c8d51c551fe486535d44101327f0b0
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 21:19:04 +0530

    u

--- sw.js
+++ sw.js
@@ -35,5 +35,4 @@ self.addEventListener('fetch', (event) => {
                                                            });
                                                                })
                                                                  );
-                                                                 });
-})
+                                                                 )
