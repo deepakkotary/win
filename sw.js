a5ae73343736e06b21da914fa1fb4b3795046898
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 20:45:54 +0530

    Project 9

--- sw.js
+++ sw.js
@@ -1,43 +1,12 @@
-
- 
-
-
-
-
-
-
-// Service Worker script
-
-const cacheName = 'myCache';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            return cache.addAll([
-                "index.html",
-                    '/',
-                    'https://deepakkotary.github.io/win/index.html',
-                    './index.html',
-                            './Index.js',
-                            './manifest.json',
-                            './icon.jpg',
-                            './icon(1).jpg'
-
-                                    // Add other files you want to cache
-                                          ]);
-                                              })
-                                                );
-                                                });
-
-                                                self.addEventListener('fetch', (event) => {
-                                                  event.respondWith(
-                                                      caches.match(event.request).then((response) => {
-                                                            return response || fetch(event.request).then((fetchResponse) => {
-                                                                    return caches.open(cacheName).then((cache) => {
-                                                                              cache.put(event.request, fetchResponse.clone());
-                                                                                        return fetchResponse;
-                                                                                                });
-                                                                                                      });
-                                                                                                          })
-                                                                                                            );
-                                                                                                            });
+self.addEventListener('install',(event)=>{
+     event.waitUntil(
+   caches.open('lala').then(Cache=>{
+        Cache.addAll(
+             [
+                  "/",
+                  "index.html"
+                  ]
+             )
+   })       
+         )
+})
