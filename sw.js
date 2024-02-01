3c66e3d2eee5a7d533741aa04d282dfac990eed7
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 21:10:38 +0530

    offline

--- sw.js
+++ sw.js
@@ -11,3 +11,23 @@ self.addEventListener('install',(event)=>{
    })       
          )
 })
+
+
+
+
+//fetching
+
+
+self.addEventListener('fetch', (event) => {
+       event.respondWith(
+           caches.match(event.request).then((response) => {
+                 return response || fetch(event.request).then((fetchResponse) => {
+                         return caches.open("lala").then((cache) => {
+                                   cache.put(event.request, fetchResponse.clone());
+                                             return fetchResponse;
+                                                     });
+                                                           });
+                                                               })
+                                                                 );
+                                                                 });
+})
