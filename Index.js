7fd70295f8fbb108e6534f91ca5a490cf63c10a2
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 21:57:29 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,30 +3,8 @@
 // PWA main script
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then((registration) => {
-    registration.addEventListener('updatefound', () => {
-      const newWorker = registration.installing;
-      newWorker.addEventListener('statechange', () => {
-        if (newWorker.state === 'installed') {
-          if (navigator.serviceWorker.controller) {
-            // New content is available, prompt the user to update
-            navigator.serviceWorker.controller.postMessage({ type: 'promptUpdate' });
-          } else {
-            console.log('Content is cached for offline use.');
-          }
-        }
-      });
-    });
-  });
-
-  let refreshing;
-  navigator.serviceWorker.addEventListener('controllerchange', () => {
-    if (refreshing) return;
-    window.location.reload();
-    refreshing = true;
-  });
+  navigator.serviceWorker.register('/sw.js');
 }
-
 
 
 
