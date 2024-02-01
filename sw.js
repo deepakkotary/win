3c88545f07cdf1b5aa72a2ea49abede51b529108
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 18:09:42 +0530

    upd	te

--- sw.js
+++ sw.js
@@ -1,22 +1,40 @@
-self.addEventListener('install',ev=>{
- ev.waitUntill(
-     caches.open('V1').then(Cache=>{
-      Cache.addAll([
-           "/",
-           "/Index.js",
-           "manifest.json",
-           "icon.jpg",
-           "icon(1).jpg"
-           
-           ])    
-          
-     }) 
-      
-      )    
-     
-})
+
+ 
 
 
 
 
 
+
+// Service Worker script
+
+const cacheName = 'myCache';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+      caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                    '/',
+                            '/Index.js',
+                            '/manifest.json',
+                            'icon.jpg',
+                            'icon(1).jpg'
+
+                                    // Add other files you want to cache
+                                          ]);
+                                              })
+                                                );
+                                                });
+
+                                                self.addEventListener('fetch', (event) => {
+                                                  event.respondWith(
+                                                      caches.match(event.request).then((response) => {
+                                                            return response || fetch(event.request).then((fetchResponse) => {
+                                                                    return caches.open(cacheName).then((cache) => {
+                                                                              cache.put(event.request, fetchResponse.clone());
+                                                                                        return fetchResponse;
+                                                                                                });
+                                                                                                      });
+                                                                                                          })
+                                                                                                            );
+                                                                                                            });
