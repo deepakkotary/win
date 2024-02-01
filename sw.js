965d8205b3910565ea31c5dd8b74a0fd0e76035f
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 17:40:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install',ev=>{
  ev.waitUntill(
      caches.open('V1').then(cache=>{
       Cache.addAll([
-           "/",
+           "https://deepakkotary.github.io/win/",
            "/Index.js",
            "manifest.json",
            "icon.jpg",
