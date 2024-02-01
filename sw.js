6b9fdaa24889e87f4535463ec90fb9e05ea448e1
deepakkotary <99587013+deepakkotary@users.noreply.github.com>
2024-02-01 21:36:58 +0530

    u

--- sw.js
+++ sw.js
@@ -38,3 +38,52 @@ self.addEventListener('fetch', (event) => {
                                                                })
                                                                  );
                                                                  });
+
+
+
+
+
+
+
+
+
+
+
+var cacheNames = "lala";
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+            return Promise.all(
+                    cacheNames.map((name) => {
+                              if (name !== cacheName) {
+                                          return caches.delete(name);
+                                                    }
+                                                            })
+                                                                  );
+                                                                      })
+                                                                        );
+                                                                        });
+
+                                                                        self.addEventListener('message', (event) => {
+                                                                          if (event.data && event.data.type === 'skipWaiting') {
+                                                                              self.skipWaiting();
+                                                                                }
+                                                                                });
+
+                                                                                // Prompt the user to update when a new version is available
+                                                                                self.addEventListener('message', (event) => {
+                                                                                  if (event.data && event.data.type === 'promptUpdate') {
+                                                                                      if (confirm('A new version is available. Do you want to update?')) {
+                                                                                            self.skipWaiting();
+                                                                                                  clients.claim();
+                                                                                                      }
+                                                                                                        }
+                                                                                                        });
+
+
+
+
+
+
