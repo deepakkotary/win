0c8accba9da1ebc486a73a7faea510d8557c654d
deepakkotary <deepakkotary.9166@gmail.com>
2024-02-02 21:53:14 +0530

    updated by acode

--- Index.js
+++ Index.js
@@ -1,10 +1,33 @@
 // registration 
 
 
+
+
+
+//updating new things
+
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/win/sw.js');
+  navigator.serviceWorker.register('/win/sw.js').then((registration) => {
+    registration.addEventListener('updatefound', () => {
+      const newWorker = registration.installing;
+      newWorker.addEventListener('statechange', () => {
+        if (newWorker.state === 'installed') {
+          if (navigator.serviceWorker.controller) {
+            // New content is available, prompt the user to update
+            navigator.serviceWorker.controller.postMessage({ type: 'promptUpdate' });
+          } else {
+            console.log('Content is cached for offline use.');
+          }
+        }
+      });
+    });
+  });
+
+  let refreshing;
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
 }
-
-
-
-
--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+var cacheName = "lala" ;
+
 self.addEventListener('install',(event)=>{
      event.waitUntil(
-   caches.open('lala').then(Cache=>{
+   caches.open(cacheName).then(Cache=>{
         Cache.addAll(
              [
 "/win",
@@ -30,7 +32,7 @@ self.addEventListener('fetch', (event) => {
        event.respondWith(
            caches.match(event.request).then((response) => {
                  return response || fetch(event.request).then((fetchResponse) => {
-                         return caches.open("lala").then((cache) => {
+                         return caches.open(cacheName).then((cache) => {
                                    cache.put(event.request, fetchResponse.clone());
                                              return fetchResponse;
                                                      });
@@ -45,6 +47,39 @@ self.addEventListener('fetch', (event) => {
 
 
 
+
+//updating new thing via uodates the contes 
+
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== cacheName) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+// Prompt the user to update when a new version is available
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'promptUpdate') {
+    if (confirm('A new version is available. Do you want to update?')) {
+      self.skipWaiting();
+      clients.claim();
+    }
+  }
+});
 
 
 
