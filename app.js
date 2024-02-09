f65ada37625f272bfbf4a2609d573ed4550e1888
deepakkotary <deepakkotary.9166@gmail.com>
2024-02-09 14:48:29 +0530

    u

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ function fuck(){
  console.log('id:',id);
 }
 
+//var tile=document.querySelector(".notesTile");
+
+
 
 //appending 
 function newDisk(){
@@ -13,7 +16,7 @@ function newDisk(){
      
   var slider = document.querySelector('.slider');
 var slide = document.querySelector('.slide');
-console.log(slide);
+//console.log(slide);
 var clone = slide.cloneNode(true);
 
    
@@ -23,7 +26,7 @@ var clone = slide.cloneNode(true);
 var addPage = document.querySelector('.addPage');
 
 // var newSlide = clone;
-slider.insertBefore(clone,addPage);
+slider.insertBefore(clone,addPage) ;
 
 
 
@@ -36,22 +39,123 @@ var newTextarea = notes.firstElementChild;
 //console.log(newTextarea);
 var tile = clone.firstElementChild;
 //console.log(tile);
-var h4 = tile.firstElementChild;
-//console.log(h4);
+var notesTile = tile.firstElementChild;
+//console.log(notesTile);
 
 
 // value zero
 newTextarea.value="";
+notesTile.value="";
+
+
+
+// store data checking in localstorage
+
+
+
 
 // id giving
 
 var f = ++i;
- var id = 't'+f ;
+ var id = 'n'+f ;
  console.log('id:',id);
+var tileId = 't'+f;
+newTextarea.setAttribute('id',id)
 
-clone.setAttribute('id',id)
-
-console.log(clone)
+notesTile.setAttribute('id',tileId);
 
 
+//console.log(clone)
+
+
+// store data in local storage
+
+var  data  = document.querySelector('#'+id);
+ console.log(data);
+
+
+
+// limited character
+
+
+}
+
+
+
+// store data in local storage
+
+function store (){
+ var  textArae  = document.querySelector('#'+id);
+ console.log(textArae);
+ var data =textArae.value;
+ console.log(data);
+ 
+ 
+ //prevention for keydown
+
+// Assume you have an event handler function
+// function handleEvent(event) {
+//   // Prevent the default behavior of the event
+//   event.preventDefault();
+// 
+//   // Your custom logic here
+//   console.log("Event prevented");
+//   
+// }
+// 
+// Attach the event handler to a specific element or document
+// tile.addEventListener("keypress", handleEvent);
+
+ 
+ 
+}
+
+
+
+// deciding to length of tile
+
+
+
+// function tileLength(){
+// var height = tile.value.split('\n').length;
+// console.log(height);
+// }
+// 
+
+
+// function limited() {
+//   var height = tile.value.split('\n').length;
+// console.log(height);
+// if(height>=3){
+//      ev.preventDefault();
+//      console.log("now, you can't write");
+//      //alert('if')
+// }else{
+//   console.log('you can write more');
+//   //alert('else');
+// }
+//   
+//    
+// }
+// 
+// 
+
+
+//prevention for keydown
+
+// //Assume you have an event handler function
+function handleEvent(event) {
+  // Prevent the default behavior of the event
+  event.preventDefault();
+
+  // Your custom logic here
+  console.log("Event prevented");
+  
+}
+
+// //Attach the event handler to a specific element or document
+function handle (){
+     tile.addEventListener("keypress", ev=>{
+          ev.preventDefault();
+     });
 }
